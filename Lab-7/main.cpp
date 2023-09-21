453783aadc1bc465b4245d3a4b79f14a67ef5a7e
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-09-20 22:09:47 -0700

    Create main.cpp

---
+++ Lab-7/main.cpp
@@ -0,0 +1,8 @@
+ // Payroll
+
+ // Design a PayRoll class that has data members for an employee’s hourly pay rate, number of hours worked of type double. The default constructor will set the hours worked and pay rate to zero. The class must have a mutator function to set the pay rate for each employee and hours worked. The class should include accessors for both the hours worked and the rate of pay. The class should lastly have a getGross function that will return a double calculated by multiplying the hours worked by the rate
+ // of pay.
+
+ // Write a program with an array of seven PayRoll objects. The program should ask the user for the rate of pay for each employee and the number of hours each employee has worked. Be sure to include an employee claiming to work more then 60 hours per week. Print out, the array number of the employee, the hours worked, the rate of pay, and the gross pay, of all the employee's each on their own line. Set the precision for printing the doubles to two decimal places.
+
+// Input Validation: Do not accept values greater than 60 for the number of hours worked, simply have the set function set number of hours worked to 60, the maximum allowed.
