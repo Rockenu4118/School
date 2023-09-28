f8d75a020a692bb66d0a4d6539afac5031f08072
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-09-28 01:23:50 -0700

    Create main.cpp

---
+++ Lab-9/main.cpp
@@ -0,0 +1,3 @@
+Design your own static Array list class to hold a series of integers. The class should have the following member function: insert, remove, isFull, find, getlength, makeEmpty and showValues.  Demonstrate the class with a driver program. 
+
+Note: You need to submit three files(specification file, implementation file and the main program file
