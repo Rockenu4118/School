183fac8116b784ea8bad4dfd19137b1fb13f6aed
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-10-02 21:37:51 -0700

    Create main.cpp

---
+++ Lab-10/main.cpp
@@ -0,0 +1,4 @@
+// Lab 10
+// Design our own linked list class to hold a series of integers. The class should have member 
+// function for appending, inserting, and deleting nodes. Do not forget to add a destructor that 
+// destroy the list. Demonstrate the class with a driver program. 
