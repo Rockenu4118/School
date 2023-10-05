901b01fa1af877fe80df627495f45e21812e1463
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-10-04 23:58:30 -0700

    Update and rename main.cpp to main.cpp

--- Lab-(10-11)/main.cpp
+++ Lab-(10-11)/main.cpp
@@ -1,5 +1,11 @@
-// Linear Search
-
+// Lab 10
+// 
+// Design our own linked list class to hold a series of integers. The class should have member 
+// function for appending, inserting, and deleting nodes. Do not forget to add a destructor that 
+// destroy the list. Demonstrate the class with a driver program. 
+//
+// Linear Search Lab 11
+//
 // Modify the linked list class you created in the previous programming Assignment. Add to  a print member fuction. 
 // The function should display all the values in the linke list. In addition, include a member function named search 
 // that returns the position of specific value, x, in the linked list. The first node in the list is at position 0, 
