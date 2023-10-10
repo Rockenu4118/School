12f59a7a5ebd47a3d614409a9481004ba802eca1
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-10-10 02:36:47 -0700

    Create main.cpp

---
+++ Lab-12/main.cpp
@@ -0,0 +1,7 @@
+Write your own version of a class that will create a stactic stack of String data type.The static stack class needs to have  the following members:
+
+1. Member Variables. stackArray ( a pointer variable which is going to be used to dynamically  allocate an array for storage), stacksize(an integer to hol the size of the stack) and top (an integer that is used to mark the top of the stack).
+
+2. Member Functions. Constructor  (it accepts an integer argument that specifies the size of the stak), Destructor, isFull, isEmpty, pop, and push. 
+
+ Demonstrate the class with a driver program.
