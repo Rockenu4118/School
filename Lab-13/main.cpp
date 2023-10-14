0bf847cd2b7a3cdb473e88356926a071487f6b3a
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-10-14 00:14:51 -0700

    Create main.cpp

---
+++ Lab-13/main.cpp
@@ -0,0 +1,6 @@
+// Write your own version of a class that will create a Dynamic Queue of 
+// double data type.The dynamic queue class needs to have  the following members:
+//
+// Member Functions. Constructor, Destructor, enqueue, dequeue, isEmpty,and clear. 
+// 
+// Demonstrate the class with a driver program.
