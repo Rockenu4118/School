534153b637638707ac1bfc25710134b07fadfb13
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-11-13 03:22:31 -0800

    Update src.asm

--- CS16-ASM/Assignment-6/src.asm
+++ CS16-ASM/Assignment-6/src.asm
@@ -41,10 +41,10 @@ DifferentInputs PROC USES ebx,			; Question 2
 	val1: DWORD,
 	val2: DWORD,
 	val3: DWORD
-	
-	mov ebx, val1
-	cmp ebx, val2
-	jz return0
+								; test all vals against eachother
+	mov ebx, val1						; if any of them are equal to eachother,
+	cmp ebx, val2						; jump to return 0
+	jz return0						; or else just run into return 1
 
 	cmp ebx, val3
 	jz return0
@@ -81,20 +81,20 @@ strConcat PROC USES eax edx edi esi,	; Question 3
 
 	L1:
 		mov al, [edi]					; move target chars into al
-		cmp al, 0						; see if end has been reached
-		jz L2							; if so, start concatenating
+		cmp al, 0					; see if end has been reached
+		jz L2						; if so, start concatenating
 				
-		inc edi							; if not, find end
+		inc edi						; if not, find end
 		jmp L1
 	L2:	
 		mov dl, [esi]					; move source string char into dl
 		mov [edi], dl					; copy over dl to target string
 
-		inc esi							; inc esi to next char from source
-		cmp BYTE PTR [esi], 0			; see if all chars have been copied
-		jz return						; if so, return
+		inc esi						; inc esi to next char from source
+		cmp BYTE PTR [esi], 0				; see if all chars have been copied
+		jz return					; if so, return
 		
-		inc edi							; if not, keep copying
+		inc edi						; if not, keep copying
 
 		jmp L2
 	return:
