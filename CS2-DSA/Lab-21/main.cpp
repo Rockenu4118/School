08c74cb1672511744ec184018132ff911790dcb1
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-11-24 23:26:20 -0800

    Create main.cpp

---
+++ CS2-DSA/Lab-21/main.cpp
@@ -0,0 +1,5 @@
+// Assignment 21
+//
+// Write a program that stores a series of integer numbers in a binary tree. 
+// Then have the program insert the values into a queue in ascending order. 
+// Dequeue the values and display them on the screen to confirm that they were stored in the proper order.
