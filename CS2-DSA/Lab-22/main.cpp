425276a9b82a523c85d27e38776b6cc5c447fba9
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-11-27 19:34:50 -0800

    Create main.cpp

---
+++ CS2-DSA/Lab-22/main.cpp
@@ -0,0 +1,23 @@
+// Assignment 22
+//
+// Employee Tree
+// Design an EmployeeInfo class that holds the following employee information:
+// Employee ID Number : int
+// Employee Name : string
+// 
+// Next, use the template you designed in Programming Challenge 1 (Binary Tree Template) to implement
+// a binary tree whose nodes hold an instance of the EmployeeInfo class. The nodes should be sorted
+// on the Employee ID number.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+  return 0;
+}
