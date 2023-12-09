ff01f9529a2547dd50300342fae1d7f80c4863fa
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2023-12-08 21:05:54 -0800

    Update main.cpp

--- CS2-DSA/Lab-23/main.cpp
+++ CS2-DSA/Lab-23/main.cpp
@@ -20,3 +20,64 @@
 // variable should be a bool. It will be set to true if the customer wishes to be on a mailing list, or 
 // false if the customer does not wish to be on a mail- ing list. Write appropriate accessor and mutator 
 // functions for these member variables. Demonstrate an object of the CustomerData class in a simple program.
+
+#include <iostream>
+
+#include "./CustomerData.hpp"
+
+using namespace std;
+
+int main()
+{
+    CustomerData customer1;
+
+    string inputStr;
+    int    inputNum;
+    char   inputChar;
+
+    cout << "Enter customer's last name: ";
+    cin >> inputStr;
+    customer1.setLastName(inputStr);
+    cout << "Enter customer's first name: ";
+    cin >> inputStr;
+    cin.ignore();
+    customer1.setLastName(inputStr);
+    cout << "Enter customer's address: ";
+    getline(cin, inputStr);
+    customer1.setAddress(inputStr);
+    cout << "Enter customer's city: ";
+    getline(cin, inputStr);
+    customer1.setCity(inputStr);
+    cout << "Enter customer's state: ";
+    getline(cin, inputStr);
+    customer1.setState(inputStr);
+    cout << "Enter customer's Zip: ";
+    cin >> inputNum;
+    customer1.setZip(inputNum);
+    cout << "Enter customer's Phone number: ";
+    cin >> inputStr;
+    customer1.setPhone(inputStr);
+    cout << "Enter customer's Customer number: ";
+    cin >> inputNum;
+    customer1.setCustomerNumber(inputNum);
+    cout << "Enter 'y' if customer is on mailing list, 'n' if not: ";
+    cin >> inputChar;
+    if (inputChar == 'y')
+        customer1.setMailingList(true);
+    else
+        customer1.setMailingList(false);
+
+    cout << "Displaying customer data."                          << endl;
+    cout << "Last name:       " << customer1.getLastName()       << endl;
+    cout << "First name:      " << customer1.getFirstName()      << endl;
+    cout << "Address:         " << customer1.getAddress()        << endl;
+    cout << "City:            " << customer1.getCity()           << endl;
+    cout << "State:           " << customer1.getState()          << endl;
+    cout << "Zip:             " << customer1.getZip()            << endl;
+    cout << "Phone number:    " << customer1.getPhone()          << endl;
+    cout << "Customer number: " << customer1.getCustomerNumber() << endl;
+
+    cout << "Mailing list:    " << (customer1.getMailingList() ? "Yes" : "No") << endl;
+
+    return 0;
+}
